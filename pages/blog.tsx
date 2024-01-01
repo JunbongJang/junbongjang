f41425210a4ddd06aa02d14922974b5865dda66f
JunbongJang <jangjunbong@gmail.com>
2024-01-01 14:22:23 +0900

    tidy refactor

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -20,26 +20,6 @@ function formatText(text: string, limitLength = 50) {
   return `${newText.toString().replaceAll(",", " ")}...`
 }
 
-
-
-const getDefaultTumbnail = function(date:any) {
-  
-  return (
-    <div className="text-center flex justify-center items-center h-full">
-      <div className="text-gray-700 dark:text-gray-400">
-        <span className="font-extrabold text-5xl md:text-7xl">
-          day
-        </span>
-        <br/>
-        <span className="text-sm sm:text-base">
-          month, year
-        </span>
-      </div>
-    </div>
-  )
-}
-
-
 export default function Blog({ blogs }) {
 
   
@@ -146,72 +126,6 @@ export default function Blog({ blogs }) {
                     </main>
                   </header>
                 </Link>
-
-                // <span>
-                // <article
-                //   key={blog.id}
-                //   className="overflow-hidden my-2 sm:py-2 sm:px-4 border-b border-gray-200 dark:border-gray-700 sm:grid sm:grid-cols-5 sm:gap-4"
-                // >
-                //     <div className="hidden sm:block relative my-auto h-0 w-[100%] pb-[100%] sm:col-span-1 cursor-pointer border border-gray-200 dark:border-gray-800 clay-1 rounded-3xl"
-                //       // onClick={() => readPost(post.slug)}
-                //     >
-                //     {blog.thumbnail ? (
-                //       <Image
-                //         src={blog.thumbnail}
-                //         className={`object-cover transition-all rounded-3xl`}
-                //         layout="fill"
-                //         alt={post.title}
-                //         loading="lazy"
-                //       />
-                //     ):(
-                //       <div className="absolute left-0 top-0 w-full h-full ">
-                //         <div className={`w-full h-full transition-all`}>
-                //           {getDefaultTumbnail(blog?.date?.start_date)}
-                //         </div>
-                //       </div>
-                //     )}
-
-                //   </div>
-                //   <div className="py-4 px-2 md:px-4 sm:col-span-4">
-                //       <div className="flex gap-2">
-                //         {blog.tags &&
-                //           blog.tags.map((tag: string, idx: number) => (
-                //             <Tag key={idx}>{tag}</Tag>
-                //           ))}
-                //       </div>
-                //       <header className="flex flex-col justify-between md:flex-row md:items-baseline">
-                //         <h2 className={`text-xl md:text-[21px] cursor-pointer font-bold transition-all`}
-                //           // onClick={() => readPost(post.slug)}
-                //           // onMouseEnter={() => {setHoverTitle(true)}}
-                //           // onMouseLeave={() => {setHoverTitle(false)}}
-                //           >
-                //           {blog.title}
-                //         </h2>
-                //       </header>
-                //       <main className="my-2 md:my-3">
-                //         <p className="leading-tight md:leading-8 text-sm md:text-[15px] dark:text-gray-400 font-normal">
-                //           {blog.summary}
-                //         </p>
-                //       </main>
-                //       <div className="flex items-center gap-2 mt-6 mb-2">
-                //         <div className="relative text-xs md:text-sm text-gray-500 dark:text-gray-400 w-full">
-                //           {formatDistanceToNow(new Date(blog.created_time), {  // blog.properties.생성일.date.start
-                //             locale: enUS,
-                //             addSuffix: true,
-                //           }) }
-                //           <span 
-                //             className="absolute top-0 right-0 cursor-pointer mr-1 mb-2 text-xs md:text-sm font-light text-sky-500 hover:text-sky-600 dark:hover:text-sky-400"
-                //             // onClick={() => readPost(post.slug)}
-                //             // onMouseEnter={() => {setHoverTitle(true)}}
-                //             // onMouseLeave={() => {setHoverTitle(false)}}
-                //           >
-                //             Read more →
-                //           </span>
-                //       </div>
-                //     </div>
-                //   </div>
-                // </article>
-                // </span>
 
               ))}
           </div>
