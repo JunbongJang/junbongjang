b2e03d5fcb4d35299d801a816c5a1f76db787926
JunbongJang <jangjunbong@gmail.com>
2024-01-01 17:32:27 +0900

    update comment section

--- components/disqus_comments.js
+++ components/disqus_comments.js
@@ -2,10 +2,13 @@ import {DiscussionEmbed} from "disqus-react"
 const DisqusComments = ({ blog }) => {
   const disqusShortname = "jjs-blog-1"
   const disqusConfig = {
-    url: "https://junbongjang.vercel.app/blog",
-    identifier: blog.id, // Single post id
-    title: blog.title // Single post title
+    url: `https://junbongjang.vercel.app/blog/${blog.blog_id}`,
+    identifier: blog.blog_id, // Single post id
+    title: blog.page_response.properties.이름.title[0].plain_text // Single post title
   }
+
+  console.log('disqusConfig', disqusConfig)
+
   return (
     <div>
       <DiscussionEmbed
